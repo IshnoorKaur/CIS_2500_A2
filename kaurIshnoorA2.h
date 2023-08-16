a5ded3ffebb89a8f3ce0ec678ae148efaa360d4e
Ishnoor Kaur <119340690+IshnoorKaur@users.noreply.github.com>
2023-08-16 19:05:43 -0400

    Update kaurIshnoorA2.h

--- kaurIshnoorA2.h
+++ kaurIshnoorA2.h
@@ -1,16 +1,3 @@
-/*
-Student Name: Ishnoor Kaur
-Course: CIS*2500
-I have exclusive control over this submission via my password.
-By including this header comment, I certify that:
-1) I have read and understood the policy on academic integrity.
-2) I have completed Moodle's module on academic integrity.
-3) I have achieved at least 80% on the academic integrity quiz
-I assert that this work is my own. I have appropriate acknowledged
-any and all material that I have used, be it directly quoted or
-paraphrased. Furthermore, I certify that this assignment was written
-by me in its entirety.
-*/
 
 #include <stdio.h>
 #include <string.h>
